5ba423f5adcd79dfc649be406c89ed20ba92fb49
Floren HENRI <fhenri@e1r5p14.42.fr>
2017-02-27 17:46:12 +0100

    [ADD] A*star is in progresse

---
+++ astar.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Heuristique struct {
+   x int
+   y int
+   count int
+   heuristioque int
+
+}
+
+type Noeu struct {
+ x int
+ y int
+}
+
+type Heu struct {
+  H int
+  G int
+}
+
+
+func checKGood( tab [][]int, goodTab[][]int, len int) bool  {
+  var x = 0
+  for x < len {
+    var y = 0
+    for y < len {
+      if tab[x][y] != goodTab[x][y] { return false }
+      y++
+    }
+    x++
+  }
+  return true
+}
+
+func  aff(tab [][]int, len int)  {
+  var x = 0
+  fmt.Printf("====================\n")
+  for x < len {
+    fmt.Printf("%d\n",tab[x])
+    x++
+  }
+  fmt.Printf("====================\n")
+
+}
+// func noeuComparaison(n1 Heuristique, n2 Heuristique) int  {
+//   if n1.heuristique < n2.heuristique  {
+//     return 1
+//   } else if n1.heuristique  == n2.heuristique {
+//     return 0
+//   } else {
+//     return -1
+//   }
+//
+// }
+//
+func calculeHeu(deplacement int, nextPos Noeu, togo Noeu) Heu  {
+  var heu Heu
+  somX =  nextPos.x - togo.x
+  somY = nextPos.y - togo.y
+  heu.H = somX - somY
+  heu.G = deplacement
+}
+
+func astarApllication(tab [][]int, at Noeu, togo Noeu) Noeu  {
+  var tmp Noeu
+  var deplacement  = 1
+  tmp.x = at.x
+  tmp.y = at.y
+  for tmp.x  != togo.x && tmp.y != togo.y {
+     u := calculeHeu(deplacement,  tmp, togo)
+     deplacement++
+  }
+  return at
+}
+
+func findToGo(tab [][]int,nbr, len int ) Noeu  {
+  var n Noeu
+  n.x = 0
+  n.y = 0
+  var x = 0
+  for x < len {
+    var y = 0
+    for y < len {
+      if tab[x][y] == nbr {
+        n.x = x
+        n.y = y
+      }
+      y++
+    }
+    x++
+  }
+  return n
+}
+
+func astar(tab [][]int, goodTab [][]int, len int) int  {
+
+  var cal = 0
+  aff(tab, len)
+  for !checKGood(tab,goodTab,len) && cal < 5  {
+    var x = 0
+    for x < len  {
+      var y = 0
+      for y < len {
+        if tab[x][y] == 0 {
+          var shouldBe  = goodTab[x][y]
+          if y + 1 < len && tab[x][y + 1] == shouldBe {
+            tab[x][y] = shouldBe
+            tab[x][y + 1] = 0
+
+            } else if y - 1 >=  0 && tab[x][y - 1] == shouldBe {
+
+              tab[x][y] = shouldBe
+              tab[x][y - 1] = 0
+
+              } else if x + 1 < len && tab[x + 1][y] == shouldBe {
+
+                tab[x][y] = shouldBe
+                tab[x + 1][y] = 0
+
+                } else if  x - 1 >= 0 && tab[x - 1][y] == shouldBe {
+
+                  tab[x][y] = shouldBe
+                  tab[x - 1][y] = 0
+                } else {
+                  var at Noeu
+                  at.x = x
+                  at.y = y
+                  togo := findToGo(tab, shouldBe, len)
+                  fmt.Printf("\n at.x = %d, at.y = %d togo.x = %d, togo.y = %d  \n",at.x,at.y,togo.x,togo.y)
+          //        astarApllication(tab, at, togo)
+                }
+              }
+              y++
+              aff(tab, len)
+              cal++
+            }
+            x++
+          }
+        }
+        fmt.Printf("nb of calcule = %d\n", cal)
+        return 0
+      }
--- main.go
+++ main.go
@@ -5,8 +5,23 @@ import (
 )
 
 func main () {
-  fmt.Printf("Welecome to Npuzzl")
+  fmt.Printf("Welecome to Npuzzl\n")
 //  tab, len := parcing()
-//  astar(tab, len)
+ tabGood :=[][]int{}
+ tabx0 := []int{1, 2, 3}
+ tabx1 := []int{8, 0, 4}
+ tabx2 := []int{7, 6, 5}
+ tabGood = append(tabGood, tabx0)
+ tabGood = append(tabGood, tabx1)
+ tabGood = append(tabGood, tabx2)
+
+ tab :=[][]int{}
+ tab0 := []int{1, 2, 0}
+ tab1 := []int{7, 5, 3}
+ tab2 := []int{8, 6, 4}
+ tab = append(tab, tab0)
+ tab = append(tab, tab1)
+ tab = append(tab, tab2)
+ astar(tab, tabGood,3)
   return
 }

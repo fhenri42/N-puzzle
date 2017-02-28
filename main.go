3797ad0a3d651082254fd3c25d2b5594998183d0
Bruno GANTELMI <bgantelm@e1r4p15.42.fr>
2017-02-28 10:19:46 +0100

    [ADD] add first step go

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
 )
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+// func parsing(str []string) ([][]int, [][]int, len int) {
+// len = str[1]
+// fmt.Printf("%d\n", len)
+// }
 
 func main () {
   fmt.Printf("Welecome to Npuzzl")
-//  tab, len := parcing()
+    dat, err := ioutil.ReadFile("/tmp/npuzzle-3.txt")
+    check(err)
+    fmt.Print("%s",dat)
+//  tabInit, tabFinal, len := parsing()
 //  astar(tab, len)
   return
 }

6e1dc25017a9de63360f12d9b0d81e930a75c299
Bruno GANTELMI <bgantelm@e1r4p12.42.fr>
2017-03-01 13:54:25 +0100

    ADD add parsing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
   "io/ioutil"
   "strings"
   "strconv"
+  "os"
 )
 
 func check(e error) {
@@ -54,25 +55,35 @@ func finalTab(len int) ([][]int) {
 }
 
 func parsing(str []string) ([][]int, [][]int, int) {
-  len, _ := strconv.Atoi(str[1])
-  //fmt.Printf("%d\n", len)
-  tabFinal := finalTab(len)
+  length, _ := strconv.Atoi(str[1])
+  //fmt.Printf("%d\n", length)
+  tabFinal := finalTab(length)
   var i = 0
   var j = 0
   var l = 0
   var k = 2
   tabInit := [][]int{}
-  for i < len {
+  for i < length {
     //fmt.Print(str[k])
-    strSplit := strings.Split(str[k], " ")
-    var tabLigne = make([]int, len)
-    for l < len {
+    strFirstSplit := strings.Split(str[k], "#")
+    strSplit := strings.Split(strFirstSplit[0], " ")
+    var tabLigne = make([]int, length)
+    taille := len(strSplit)
+    for j < taille {
       if strSplit[j] != "" {
-        nb,_ := strconv.Atoi(strSplit[j])
+        nb,err := strconv.Atoi(strSplit[j])
+        if err != nil {
+          fmt.Print("File is not well formated3\n")
+          os.Exit(3)
+        }
         tabLigne[l] = nb
         l++
       }
         j++
+    }
+    if l != length {
+      fmt.Print("File is not well formated2\n")
+      os.Exit(3)
     }
     l = 0
     j = 0
@@ -80,26 +91,35 @@ func parsing(str []string) ([][]int, [][]int, int) {
     k++
     i++
   }
-
-
-  return tabInit, tabFinal, len
+  return tabInit, tabFinal, length
 }
 
 func main () {
-    dat, err := ioutil.ReadFile("/tmp/npuzzle-12.txt")
+    dat, err := ioutil.ReadFile("/tmp/npuzzle-4-u-test.txt")
+    if err != nil {
+      fmt.Print("No such file or directory")
+      os.Exit(3)
+    }
     check(err)
+
     datString := string(dat)
-    //fmt.Print(datString)
     datSplitted := strings.Split(datString, "\n")
+    _, erreur := strconv.Atoi(datSplitted[1]);
+    if datSplitted != nil && datSplitted[0][0] != '#' || erreur != nil {
+        fmt.Printf("File is not well formated\n")
+        os.Exit(3)
+    }
+
     tabInit, tabFinal, len := parsing(datSplitted)
     fmt.Printf("\n%d\n", len)
     fmt.Printf("\nFirst:\n%d\n%d\n%d\n", tabInit[0][0], tabInit[0][1], tabInit[0][2])
-    fmt.Printf("\nSecond:\n%d\n%d\n%d\n%d\n", tabInit[1][0], tabInit[1][1], tabInit[1][2], tabInit[1][3])
+    fmt.Printf("\nSecond:\n%d\n%d\n%d\n", tabInit[1][0], tabInit[1][1], tabInit[1][2])
     fmt.Printf("\nThird:\n%d\n%d\n%d\n", tabInit[2][0], tabInit[2][1], tabInit[2][2])
 
     fmt.Printf("\nFirst:\n%d\n%d\n%d\n", tabFinal[0][0], tabFinal[0][1], tabFinal[0][2])
-    fmt.Printf("\nSecond:\n%d\n%d\n%d\n", tabFinal[10][0], tabFinal[9][0], tabFinal[8][0])
-    fmt.Printf("\nThird:\n%d\n%d\n%d\n", tabFinal[11][11], tabFinal[11][10], tabFinal[11][0])
+    fmt.Printf("\nSecond:\n%d\n%d\n%d\n", tabFinal[1][0], tabFinal[1][1], tabFinal[1][2])
+    fmt.Printf("\nThird:\n%d\n%d\n%d\n", tabFinal[2][0], tabFinal[2][1], tabFinal[2][2])
+    os.Exit(3)
 //  astar(tab, len)
   return
 }
